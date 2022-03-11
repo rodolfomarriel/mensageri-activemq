2df8323238a4faa321242e4a54c4f485d344363c
Rodolfo Marriel <rodolfo.29r@gmail.com>
2022-03-10 22:07:37 -0300

    Acrescentando infos ao .md e renomeando arquivos

--- README.md
+++ README.md
@@ -12,6 +12,10 @@ docker run -p 61616:61616 -p 8161:8161 rmohr/activemq
 ````
 Link de acesso ao activeMQ [http://localhost:8161](http://localhost:8161/index.html)
 
+Link de acesso ao banco local h2 http://localhost:8080/h2-console/
+
+Link de acesso a pagina de cadastro de informacoes http://localhost:8081
+
 As informacoes seguem o seguinte estilo:
 ````
 {
--- activemq-consumer/src/main/java/com/example/activemq/consumer/ConsumerApplication.java
+++ activemq-consumer/src/main/java/com/example/activemq/consumer/ConsumerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ActivemqConsumerApplication {
+public class ConsumerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ActivemqConsumerApplication.class, args);
+		SpringApplication.run(ConsumerApplication.class, args);
 	}
 
 }
